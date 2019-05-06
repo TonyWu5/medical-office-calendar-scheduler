9b3a679079188d5e9f824b4d590ccfdfc03ec1ab
Tony <tnywu@ucdavis.edu>
2019-05-06 00:33:37 -0700

    added stylesheet

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 
 app.use(express.static(__dirname + '/../client/dist'))
+
 app.get('/', (req, res) => res.send('hello, get request received!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
